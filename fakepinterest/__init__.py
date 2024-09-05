0aa47727a25bacfcf9bbd9cadf85d4a004b61e5c
alira1984 <anderlira@yahoo.com.br>
2024-09-05 00:41:56 -0300

    Criado banco postgresql

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -2,11 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
-
+import os
 
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"  # Configuração para banco de dados
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")  # Configuração para banco de dados
 app.config["SECRET_KEY"] = "5299b6dc3e8f9af13324341b873c4880"  # Configuração para senha de login
 app.config["UPLOAD_FOLDER"] = "static/fotos_posts"   # Faz o upload das fotos que o usuário enviar.
 
